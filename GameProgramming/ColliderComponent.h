20d0c37314f843fb462c700a79009d2184cb94e3
Sascha-Ruehrup <sascha.ruehrup@gmail.com>
2021-07-06 01:51:16 +0200

    Circle Rect collision

--- GameProgramming/ColliderComponent.h
+++ GameProgramming/ColliderComponent.h
@@ -7,6 +7,7 @@ class ColliderComponent : public Component
 {
 public:
 	SDL_Rect collider;
+	Circle colliderCircle;
 	std::string tag;
 
 	TransformComponent* transform;
@@ -22,6 +23,21 @@ public:
 		collider.x = xpos;
 		collider.y = ypos;
 		collider.h = collider.w = size;
+	}
+	ColliderComponent(std::string t, int xpos, int ypos, int size, bool circle)
+	{
+		if (circle) {
+			tag = t;
+			colliderCircle.x = xpos;
+			colliderCircle.y = ypos;
+			colliderCircle.r = size;	//radius
+		}
+		else {
+			tag = t;
+			collider.x = xpos;
+			collider.y = ypos;
+			collider.h = collider.w = size;
+		}
 	}
 	void init() override
 	{
@@ -39,6 +55,10 @@ public:
 			collider.y = static_cast<int>(transform->position.y);
 			collider.w = transform->width * transform->scale;
 			collider.h = transform->height * transform->scale;
+		}
+		else if (tag == "rocketlauncherprojectile") {			// maybe not needed
+			colliderCircle.x = static_cast<int>(transform->position.x);
+			colliderCircle.y = static_cast<int>(transform->position.y);
 		}
 	}
 
